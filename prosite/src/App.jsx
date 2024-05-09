4cc7366e2622a7bfc123b1ee9d8969eb26f20c2d
devJawad1 <adekanbija1@gmail.com>
2024-05-09 12:04:43 +0100

    added router

--- prosite/src/App.jsx
+++ prosite/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landingpage from './pages/Landingpage'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Landingpage/>
+    <Routes>
+      <Route path='/' element={<Landingpage/>}/>
+    </Routes>
     </>
   )
 }
